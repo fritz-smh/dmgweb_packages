e39a77c27c5d06b04542b8927e3ed5bbe0ebc00b
fritz-smh <fritz.smh@gmail.com>
2014-07-28 23:17:33 +0200

    Add 404 and 500 pages

--- application.py
+++ application.py
@@ -152,6 +152,15 @@ def after_request(response):
 
 
 
+### Errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 
 ### Views
