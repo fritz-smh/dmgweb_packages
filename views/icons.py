8abbbed143dc10e31e27b1fd766b10989edff461
fritz-smh <fritz.smh@gmail.com>
2017-09-07 17:05:32 +0200

    icon : fix

--- views/icons.py
+++ views/icons.py
@@ -4,14 +4,14 @@ from flask import render_template, render_template_string
 from dmgweb_packages.application import app#, github
 import os
 
-@app.route('/icons/<type>/<name>', methods = ['GET'])
-def icons(type, name):
+@app.route('/icon/<type>/<name>', methods = ['GET'])
+def icon(type, name):
     app.logger.debug(u"Get icon for '{0}-{1}'".format(type, name))
     filename = "{0}-{1}.png".format(type, name)
     return send_from_directory('{0}/../data/icons/'.format(os.path.dirname(os.path.realpath(__file__))), filename)
 
-@app.route('/icons/<type>/<name>/<release>', methods = ['GET'])
-def icons2(type, name, release):
+@app.route('/icon/<type>/<name>/<release>', methods = ['GET'])
+def icon_for_release(type, name, release):
     filename = "{0}-{1}-{2}.png".format(type, name, release)
     return send_from_directory('{0}/../data/icons/'.format(os.path.dirname(os.path.realpath(__file__))), filename)
 
